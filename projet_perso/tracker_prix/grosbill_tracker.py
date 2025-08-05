8671bacb0c524aeb40fe27828e9d944b2b19178c
seb-alliot <sebastienalliot04@gmail.com>
2025-08-05 23:50:00 +0200

    modif di header pour amazon, le referer fausse le prix

--- projet_perso/tracker_prix/grosbill_tracker.py
+++ projet_perso/tracker_prix/grosbill_tracker.py
@@ -9,7 +9,6 @@ def grosbill_prix_tracker():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0",
         "Accept-Language": "fr-FR,fr;q=0.9",
-        "Referer": "https://www.google.com/",
         "DNT": "1",
         "Connection": "keep-alive",
     }
